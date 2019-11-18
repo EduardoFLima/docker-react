f06b310e07a1a0f4cb9ea73a1812901b6ab82580
Edu Lima <eduardo.detominilima@metronom.com>
2019-11-18 14:48:30 +0100

    feature branch test 2

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Changed in the Feature Branch.
+          Changed in the Feature Branch again.
         </p>
         <a
           className="App-link"
